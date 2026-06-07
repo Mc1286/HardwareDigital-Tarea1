0811277925503ba1db9b09ff9e7e45e75117c75d
juan pablo Villarroel <juan.villarroela@estudiantes.uv.cl>
2026-06-07 18:48:56 -0400

    Add button input and alarm silencing feature
    Introduces a digital input for a button and a variable to manage alarm silencing. Adjusts buzzer behavior based on the alarm state.

--- monitoreo_termico.ino
+++ monitoreo_termico.ino
@@ -1,16 +1,18 @@
-//Descripción: Se añade el segundo actuador exigido por la tarea. El buzzer se acopla a la lógica del umbral de temperatura para sonar simultáneamente con el LED rojo.
+//Descripción: Se introduce la entrada digital (INPUT_PULLUP) y la variable alarmaSilenciada. En esta versión preliminar, la lectura se hace de forma directa (sin debouncing) y el programa aún depende de la función delay().
 
-// --- 1. Definición de pines ---
 const int pinTemp   = A0;
+const int pinBoton  = 3;
 const int pinBuzzer = 8;
 const int ledRojo   = 6;
 const int ledAzul  = 5;
 const int ledVerde   = 4;
 
 float limiteTemperatura = 31.0;
+bool alarmaSilenciada = false;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(pinBoton, INPUT_PULLUP);
   pinMode(pinBuzzer, OUTPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
@@ -24,15 +26,26 @@ void loop() {
   float voltaje = lectura * (5.0 / 1023.0);
   float temperaturaC = (voltaje - 0.5) * 100.0;
 
-  if (temperaturaC >= limiteTemperatura) {
-    setColor(HIGH, LOW, LOW);
-    tone(pinBuzzer, 1000);    // Activar sonido
-  } else {
-    setColor(LOW, HIGH, LOW);
-    noTone(pinBuzzer);        // Silenciar sonido
+  // Lectura básica del botón (sin antirrebote)
+  if (digitalRead(pinBoton) == LOW) {
+    alarmaSilenciada = true;
   }
 
-  delay(500);
+  if (temperaturaC >= limiteTemperatura) {
+    if (!alarmaSilenciada) {
+      setColor(HIGH, LOW, LOW); 
+      tone(pinBuzzer, 1000);
+    } else {
+      setColor(HIGH, LOW, LOW); // LED fijo indicador de falla
+      noTone(pinBuzzer);        // Buzzer silenciado manualmente
+    }
+  } else {
+    setColor(LOW, HIGH, LOW);
+    noTone(pinBuzzer);
+    alarmaSilenciada = false;   // Rearme automático
+  }
+
+  delay(200);
 }
 
 void setColor(int rojo, int verde, int azul) {
